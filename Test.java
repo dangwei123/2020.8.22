ceb05fabdd890856be603e3648db8849d699cf3c
党威 <1357711677@qq.com>
2020-08-22 14:18:57 +0800

    Test.java

---
+++ Test.java
@@ -0,0 +1,64 @@
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> list=new ArrayList<>();
+        ListNode cur=head;
+        while(cur!=null){
+            list.add(cur.val);
+            cur=cur.next;
+        }
+        return transform(list,0,list.size()-1);
+    }
+    private TreeNode transform(List<Integer> list,int left,int right){
+        if(left>right) return null;
+        int mid=left+(right-left)/2;
+        TreeNode root=new TreeNode(list.get(mid));
+        root.left=transform(list,left,mid-1);
+        root.right=transform(list,mid+1,right);
+        return root;
+    }
+}
